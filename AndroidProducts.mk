206f1a7261a2ea7f16cbd382532493cce039734a
Ahmed Shahir Samin <ashahir05@gmail.com>
2023-10-22 13:05:22 +0600

    Init twrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a12s.mk
+    $(LOCAL_DIR)/lineage_a12s.mk \
+    $(LOCAL_DIR)/twrp_a12s.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_a12s-user \
     lineage_a12s-userdebug \
-    lineage_a12s-eng
+    lineage_a12s-eng \
+    twrp_a12s-user \
+    twrp_a12s-userdebug \
+    twrp_a12s-eng
